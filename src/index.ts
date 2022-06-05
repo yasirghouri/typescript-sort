99cdd0ebc125cbbe016797edeea7437c25a192c0
yasirghouri <yasir.ag7@gmail.com>
2022-06-05 16:52:30 +0500

    made Sorter class abstract to refactor code.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 import { NumbersCollection } from "./NumbersCollection";
-import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([5, 2, 8, 1, -9, 100]);
-const numberSorter = new Sorter(numbersCollection);
-numberSorter.sort();
+numbersCollection.sort();
 
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("zaRs");
-const characterSorter = new Sorter(charactersCollection);
-characterSorter.sort();
+charactersCollection.sort();
 
 console.log(charactersCollection.data);
 
@@ -20,7 +17,6 @@ linkedList.add(2);
 linkedList.add(1);
 linkedList.add(7);
 linkedList.add(-8);
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+linkedList.sort();
 
 linkedList.print();
