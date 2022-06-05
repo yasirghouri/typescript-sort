cf010dedd69f1592d3c88397b9691dee11a91547
yasirghouri <yasir.ag7@gmail.com>
2022-06-05 16:09:57 +0500

    added sorting for number[] as well as string

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([5, 2, 8, 1, -9, 100]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numberSorter = new Sorter(numbersCollection);
+numberSorter.sort();
 
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("zars");
+const characterSorter = new Sorter(charactersCollection);
+characterSorter.sort();
+
+console.log(charactersCollection.data);
